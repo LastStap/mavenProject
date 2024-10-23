b80a1ef476c42fc7571411276216ef285c58bf11
Daniil Dumshenko <dumshenko2003@gmail.com>
2024-10-23 08:33:55 +0200

    completed homework 13

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,5 +4,23 @@ import java.util.Scanner;
 
 public class App
 {
+    public static int fibonacci(int n) {
 
+        if (n == 0) {
+            return 0;
+        } else if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введіть порядковий номер числа Фібоначчі: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Fibonacci of " + n + " is: " + fibonacci(n));
+    }
 }
